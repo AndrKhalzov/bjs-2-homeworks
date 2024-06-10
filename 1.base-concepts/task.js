a130ae35100b991b2f6ff097845ee280c1c3c31f
Khalzov <khalzov.andr@gmail.com>
2024-06-10 19:45:13 +0300

    Реализация первого задания

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,23 @@
 "use strict"
 function solveEquation(a, b, c) {
+  let diskr = b ** 2 - 4 * a * c;
   let arr = [];
-  
+  if (diskr < 0) {
+    arr = [];
+  } else if (diskr === 0); {
+    arr = [-b / (2 * a)];
+  } else if (diskr > 0) {
+    arr = [(-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)]
+  }
   return arr;
 }
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+
+ function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+  percent / 100;
+  let kredit = amount - contribution;
+  let monthlyPercent = percent / 12;
+  let monthlyPayment = kredit * (monthlyPercent + (monthlyPercent / (((1 + monthlyPercent) ** countMonths) - 1)));
+  let totalAmount = kredit + (monthlyPayment * 12);
+  return +totalAmount.toFixed(2);
 }
