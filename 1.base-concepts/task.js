6a7850c01a6a33184c128437237b0fda4d0c1c5d
Khalzov <khalzov.andr@gmail.com>
2024-06-11 09:55:09 +0300

    Выполнение 1 задания финал

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -4,11 +4,11 @@ function solveEquation(a, b, c) {
   let d = b ** 2 - 4 * a * c;
   
   if (d === 0) {
-    x1 = [ - b / (2 * a)];
+    x1 = - b / (2 * a);
     arr.push(x1);
   } else if (d > 0) {
-    x1 = [( - b + Math.sqrt(d)) / (2 * a)];
-    x2 = [( - b - Math.sqrt(d)) / (2 * a)];
+    x1 = ( - b + Math.sqrt(d)) / (2 * a);
+    x2 = ( - b - Math.sqrt(d)) / (2 * a);
     arr.push(x1, x2);
   }
   return arr;
@@ -17,7 +17,7 @@ function solveEquation(a, b, c) {
 
  function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   let kredit = amount - contribution;
-  let monthlyPercent = (percent / 12) / 100;
+  let monthlyPercent = (percent / 12) / 10;
   let monthlyPayment = kredit * (monthlyPercent + (monthlyPercent / (((1 + monthlyPercent) ** countMonths) - 1)));
   let totalAmount = kredit + (monthlyPayment * 12);
   return +totalAmount.toFixed(2);
