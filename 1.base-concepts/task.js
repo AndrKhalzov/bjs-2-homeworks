efba74239b799df678507ecacbc010c02ad07f61
Khalzov <khalzov.andr@gmail.com>
2024-06-11 18:43:58 +0300

    последние изменения

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,14 +1,17 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  let x1;
-  let x2;
+  
+
   let d = b ** 2 - 4 * a * c;
   
   if (d === 0) {
+    let x1;
     x1 = - b / (2 * a);
     arr.push(x1);
   } else if (d > 0) {
+    let x1;
+    let x2;
     x1 = ( - b + Math.sqrt(d)) / (2 * a);
     x2 = ( - b - Math.sqrt(d)) / (2 * a);
     arr.push(x1, x2);
@@ -21,6 +24,6 @@ function solveEquation(a, b, c) {
   let kredit = amount - contribution;
   let monthlyPercent = (percent / 12) / 100;
   let monthlyPayment = kredit * (monthlyPercent + (monthlyPercent / (((1 + monthlyPercent) ** countMonths) - 1)));
-  let totalAmount = kredit + (monthlyPayment * 12);
+  let totalAmount = monthlyPayment * countMonths;
   return +totalAmount.toFixed(2);
 }
