f660ab84b7dd0dfdfc013da4777cac1dcce1f402
Khalzov <khalzov.andr@gmail.com>
2024-07-04 14:28:41 +0300

    дз объекты

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,36 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
+
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
-}
+  if (this.marks === undefined) {
+    return 0;
+    } else { 
+      return this.marks.push(...marks);
+    }
+  }
 
 Student.prototype.getAverage = function () {
+  if (this.marks === undefined || this.marks.length === 0) {
+    return 0;
+} else {
+  return this.marks.reduce( ( acc, item ) => acc + item) / this.marks.length;
+  }
+}
   
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
 }
 
-Student.prototype.exclude = function (reason) {
-  
-}
+let student = new Student("Tony", "male", 37);
+console.log(student.getAverage());
